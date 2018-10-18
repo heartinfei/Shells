c0c83622c022c92fedae30cc202a5d4f2724ab61
王强 <249346528@qq.com>
2018-10-18 18:02:27 +0800

    brew_install update

--- brew_install.sh
+++ brew_install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ -e "/usr/local/bin/brew" ];then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "brew already installed."
+fi
